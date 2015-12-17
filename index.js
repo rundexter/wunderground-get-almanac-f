fd12b74cee5752eda62b174c461337e0bacddb1a
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-17 11:36:11 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
+var _ = require('lodash'),
+    request = require('request'),
+    util = require('./util.js');
+
+var apiURL = 'http://api.wunderground.com/api/';
+
+var pickInputs = {
+        'country': 'country',
+        'city': 'city'
+    },
+    pickOutputs = {
+        'almanac_airport_code': 'almanac.airport_code',
+        'temp_high_normal': 'almanac.temp_high.normal.F',
+        'temp_high_record': 'almanac.temp_high.record.F',
+        'temp_high_recordyear': 'almanac.temp_high.recordyear',
+        'temp_low_normal': 'almanac.temp_low.normal.F',
+        'temp_low_record': 'almanac.temp_low.record.F',
+        'temp_low_recordyear': 'almanac.temp_low.recordyear'
+    };
+
 module.exports = {
+
+    /**
+     * Get auth data.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authorizeRequest: function (dexter) {
+
+        if(!dexter.environment('wunderground_api_key')) {
+
+            this.fail('A [wunderground_api_key] environment variable is required for this module');
+
+            return false;
+        } else {
+
+            request = request.defaults({
+                baseUrl: apiURL.concat(_(dexter.environment('wunderground_api_key')).toString().trim())
+            });
+
+            return true;
+        }
+    },
+
+    /**
+     * Check correct inputs data.
+     *
+     * @param step
+     * @param pickInputs
+     * @returns {*}
+     */
+    checkInputStruct: function (step, pickInputs) {
+        var requestData = util.pickStringInputs(step, pickInputs);
+
+        if (!requestData.country || !requestData.city) {
+
+            this.fail('A [country,city] inputs need for this module.');
+
+            return false;
+        }
+
+        return requestData;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +70,24 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var requestData = this.checkInputStruct(step, pickInputs),
+            uri = 'almanac/q/' + requestData.country + '/' + requestData.city + '.json';
+
+        if (!this.authorizeRequest(dexter) || !requestData)
+            return;
+
+
+        request.get({uri: uri, json: true}, function (error, response, data) {
+
+            if (error)
+                this.fail(error);
+
+            else if (response.statusCode !== 200)
+                this.fail(response.statusCode + ': Something is happened');
+
+            else
+                this.complete(util.pickResult(data, pickOutputs));
+        }.bind(this));
     }
 };
